c8400798ee881b62d4d0214df26da68fe15e4b70
Bhimkc90 <bhim.b.kc90@gmail.com>
2026-06-16 02:26:34 -0400

    Initial Express and MongoDB Atlas setup

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    userType: {
+      type: String,
+      enum: ["military", "non-military"],
+      required: true,
+    },
+
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
