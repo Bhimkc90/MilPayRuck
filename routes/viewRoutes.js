76285f382e9b7eed1e9ff3987702e19e3abf854e
Bhimkc90 <bhim.b.kc90@gmail.com>
2026-06-22 19:54:17 -0400

    Add dashboard, profile, budget and transaction pages

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+} = require("../controllers/authController");
 
 router.get("/register", (req, res) => {
   const userType = req.query.type || "military";
@@ -11,6 +14,27 @@ router.get("/register", (req, res) => {
   });
 });
 
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard/dashboard");
+});
+
+router.get("/profile", (req, res) => {
+  res.render("profile/profile");
+});
+
+router.get("/budget", (req, res) => {
+  res.render("budget/budget");
+});
+
+router.get("/transactions", (req, res) => {
+  res.render("transactions/transactions");
+});
+
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
