527149e2825ca527dcb08b0093917e590df100e7
Bhimkc90 <bhim.b.kc90@gmail.com>
2026-06-22 19:59:20 -0400

    Add get transactions endpoint

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -15,6 +15,21 @@ const createTransaction = async (req, res) => {
   }
 };
 
+const getTransactionsByUserId = async (req, res) => {
+  try {
+    const transactions = await Transaction.find({
+      userId: req.params.userId,
+    }).sort({ transactionDate: -1 });
+
+    res.json(transactions);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   createTransaction,
+  getTransactionsByUserId,
 };
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
   createTransaction,
+  getTransactionsByUserId,
 } = require("../controllers/transactionController");
 
 router.post("/", createTransaction);
+router.get("/:userId", getTransactionsByUserId);
 
 module.exports = router;
