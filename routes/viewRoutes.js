5316902dfbdab1cd8944be7be329fa88d3f002b4
Bhimkc90 <bhim.b.kc90@gmail.com>
2026-06-22 20:23:04 -0400

    Display real budget calculations on dashboard

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Transaction = require("../models/Transaction");
+const Budget = require("../models/Budget");
+const Profile = require("../models/Profile");
 
 const {
   registerUser,
@@ -19,17 +21,55 @@ router.get("/login", (req, res) => {
   res.render("auth/login");
 });
 
-router.get("/dashboard", (req, res) => {
-  res.render("dashboard/dashboard");
+
+
+
+router.get("/dashboard", async (req, res) => {
+  const userId = "6a30f17842ab4a2c8fc3120b";
+
+  const budget = await Budget.findOne({ userId }).lean();
+
+  let totalExpenses = 0;
+
+  for (let category in budget.expenses) {
+    totalExpenses += budget.expenses[category];
+  }
+
+  const remainingCash =
+    budget.monthlyNetIncome - totalExpenses;
+
+  const savingsRate =
+    ((budget.expenses.savings + budget.expenses.investments) /
+      budget.monthlyNetIncome) *
+    100;
+
+  res.render("dashboard/dashboard", {
+    income: budget.monthlyNetIncome,
+    totalExpenses,
+    remainingCash,
+    savingsRate: savingsRate.toFixed(1),
+  });
 });
 
 router.get("/profile", (req, res) => {
   res.render("profile/profile");
 });
 
-router.get("/budget", (req, res) => {
-  res.render("budget/budget");
+
+
+
+router.get("/budget", async (req, res) => {
+  const userId = "6a30f17842ab4a2c8fc3120b";
+
+  const budget = await Budget.findOne({ userId }).lean();
+
+  res.render("budget/budget", {
+    income: budget.monthlyNetIncome,
+    expenses: budget.expenses,
+  });
 });
+
+
 
 router.get("/transactions", async (req, res) => {
   const userId = "6a30f17842ab4a2c8fc3120b";
